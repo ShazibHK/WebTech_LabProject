ed4da189f54b6cb94ed29454a1e0a36e87c7bcab
Shawn11a <dshawn1109@gmail.com>
2021-05-28 20:46:45 +0530

    user dashboard ui react

---
+++ User_panel_React/my-app/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Heading from './Heading';
+import Mobilesidepanel from './Sidepanel';
+import Panel from './Panel';
+function App(){
+
+    return <div> 
+            <Heading /> 
+            <Mobilesidepanel />
+            <Panel />
+            
+           </div>
+}
+
+export default App;
